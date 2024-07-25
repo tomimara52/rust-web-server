05a8b7ef62b7a09fc08c657ac22f8d3d13ae0cf0
Tomás Maraschio <tomas.maraschio@gmail.com>
2024-07-25 19:43:07 -0300

    get_handler logic

--- src/router.rs
+++ src/router.rs
@@ -1,19 +1,23 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, iter::zip};
 
-use regex::RegexSet;
+use regex::{Regex, RegexSet};
+
+use crate::handler::{Handler, Params};
 
 pub struct Router {
-    pub routes: RegexSet,
+    routes: RegexSet,
+    handlers: Vec<&'static dyn Handler>,
 }
 
 impl Router {
     pub fn new() -> Router {
         Router { 
             routes: RegexSet::empty(),
+            handlers: Vec::new(),
         }
     }
 
-    pub fn add_route(&mut self, path: &str) {
+    pub fn add_route(&mut self, path: &str, handler: &'static dyn Handler) {
         assert!(path.len() > 0 && path.chars().nth(0).unwrap() == '/');
 
         let mut regex = String::from("^");
@@ -48,6 +52,31 @@ impl Router {
 
         let mut patterns = self.routes.patterns().to_vec();
         patterns.push(regex);
-        self.routes = RegexSet::new(patterns).expect("Error when compiling regex")
+        self.routes = RegexSet::new(patterns).expect("Error when compiling regex");
+        
+        self.handlers.push(handler);
+    }
+
+    pub fn get_handler(&self, path: &str) -> Option<(&'static dyn Handler, Params)> {
+        let matches: Vec<_> = self.routes.matches(path).into_iter().collect();
+        
+        if matches.len() == 0 {
+            return None;
+        }
+
+        let regex = Regex::new(&self.routes.patterns()[matches[0]]).unwrap();
+        let caps = regex.captures(path)?;
+        let names = regex.capture_names().skip(1);
+
+        let mut params = HashMap::new();
+
+        for (name, cap) in zip(names, caps.iter().skip(1)) {
+            params.insert(name?.to_string(), cap?.as_str().to_string());
+        }
+
+        Some((
+            self.handlers[matches[0]],
+            params
+        ))
     }
 }
